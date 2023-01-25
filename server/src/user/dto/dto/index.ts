05d81c9bcf8964deb77b457cfa768637e7d64e0f
Ruben Ghambaryan <rubenghambaryan@Rubens-MacBook-Pro.local>
2023-01-25 16:42:16 +0400

    Add Test Project

---
+++ server/src/user/dto/dto/index.ts
@@ -0,0 +1,7 @@
+import * as _inputs from "./input/index"
+// import * as _outputs from "./output/index"
+
+export namespace userDto {
+  export import inputs = _inputs
+  // export import outputs = _outputs
+}
