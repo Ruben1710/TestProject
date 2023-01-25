64ac31b278f5be14e16d5d0e17f785974ad2167c
Ruben1710 <rubenghambaryan1@gmail.com>
2023-01-25 17:14:01 +0400

    x

--- test/server/src/user/dto/dto/index.ts
+++
@@ -1,7 +0,0 @@
-import * as _inputs from "./input/index"
-// import * as _outputs from "./output/index"
-
-export namespace userDto {
-  export import inputs = _inputs
-  // export import outputs = _outputs
-}
